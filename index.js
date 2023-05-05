d7114d6c3d01bf4b599aed98489a1958e840170f
xiAMLEO <leonardobonanni002@gmail.com>
2023-05-05 23:08:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const fs = require("fs");
 const app = express();
 const data = JSON.parse(fs.readFileSync("quotes.json"));
 
+const PORT = 8080;
+
 app.use(express.json());
 app.use(cors());
 
-app.listen(8080, (req, res) => {
-  console.log("Listening on port 8080");
+app.listen(PORT, (req, res) => {
+  console.log(`Listening on port ${PORT}`);
 });
 
 app.get("/api", (req, res) => {
@@ -20,8 +22,6 @@ app.get("/api", (req, res) => {
   const quotesLength = parseInt(
     Object.keys(data[Object.keys(data)[randomMovie]]).length
   );
-
-  console.log(quotesLength);
 
   const randomQuote = parseInt(Math.random() * Object.keys(quotesLength - 1));
 
